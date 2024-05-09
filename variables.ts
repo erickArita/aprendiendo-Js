d225b2dbe3a31ce6766406a22a1947ab21fd80f1
Erick Arita <erick.emao@gmail.com>
2024-05-08 20:46:56 -0600

    variables

---
+++ variables.ts
@@ -0,0 +1,11 @@
+var variable = "Hello World!";
+const constant = "Hello World!";
+
+let letVariable = "Hello World!";
+
+function test(arg1, arg2) {
+  console.log(arg1, arg2);
+}
+const suma = (arg1, arg2) => {
+  return arg1 + arg2;
+};
